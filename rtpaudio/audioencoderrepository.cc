e585d7ab099c7086cea28e731ad1f42940fd5532
dreibh <dreibh@acdd8558-d4fd-0310-b0a5-8cf3c1ec824a>
2011-12-12 13:47:26 +0000

    Fixed uninitialized variable.
    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp-misc/trunk/rtpaudio2@1195 acdd8558-d4fd-0310-b0a5-8cf3c1ec824a

--- rtpaudio/audioencoderrepository.cc
+++ rtpaudio/audioencoderrepository.cc
@@ -39,7 +39,8 @@
 // ###### Constructor #######################################################
 AudioEncoderRepository::AudioEncoderRepository()
 {
-   Encoder = NULL;
+   Encoder    = NULL;
+   AutoDelete = true;
 }
 
 
