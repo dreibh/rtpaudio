ff6281e9db36cf4685b3ee86b310fdc64049894f
Thomas Dreibholz <dreibh@simula.no>
2017-03-25 23:16:10 +0100

    Build script updates.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ./bootstrap && \
-./configure --enable-kernel-sctp --enable-static --disable-shared --enable-qt $@ && \
+./configure --enable-kernel-sctp --enable-static --disable-shared --enable-qt --enable-pulseaudio $@ && \
 ( gmake -j2 || make -j2 )
